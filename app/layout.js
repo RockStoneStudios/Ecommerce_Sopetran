a4ca7b869a4cb67e1faab0fe3b258ce306604efd
OMAR <rockstonestudios666@gmail.com>
2024-05-02 12:47:43 -0500

    remove @context

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "../styles/main.scss";
-import Providers from "@/context/Providers";
+// import Providers from "@/context/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
